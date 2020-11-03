fee4a50748172120e816c406b859d2c45a9dcb67
Trevor <tlow94@uw.edu>
2020-11-03 15:36:19 -0800

    JWT to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,25 @@ class UsersController < ApplicationController
     before_action :authenticate, only: [:update, :destroy]
     
     def index
+        users = User.all;
+        render json: {user: users}
     end
 
     def new
     end
 
     def create
-    end
+        secret_key = Rails.application.secrets.secret_key_base[0];
+        @user = User.create(user_params)
+          if @user.valid?
+            @token = JWT.encode({user_id: @user.id,
+              username: @user.username
+          }, secret_key)
+            render json: { user: User.all, token: @token }, status: :created
+          else
+            render json: {error: "Failed to create a user"}
+          end
+        end
 
     def show
     end
@@ -20,6 +32,12 @@ class UsersController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :birthday, :bio, :password)
     end
 
 end
